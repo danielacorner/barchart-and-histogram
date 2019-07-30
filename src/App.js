3d870945800d1027d752b4c4dd58d844d996d527
Daniel Corner <daniel@raventelemetry.com>
2019-07-30 13:28:33 -0400

    add Histogram

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BarChart from './components/BarChart';
 import exampleData from './data';
+import Histogram from './components/Histogram';
 
-// TODO 2: TagBreakdownChart: render one bar for each tag in each category
-// for a responsive height, we set the container width and height, then set the bar dimensions with % units
-// use d3.scale to create the scale function (domain = min(bars[tagName].time), range = 0, 100)
-// https://github.com/d3/d3-scale
-// var x = d3.scaleLinear()
-//     .domain([10, 130]) // domain = input
-//     .range([0, 960]);  // range  = output
-
-// x(20); // 80
-// x(50); // 320
-
-// const scaleY = d3.scaleLinear()
-//                  .domain([0,Math.max(...bars.map(bar=>bar.time))])
-//                  .range([0,50])
-// const TagBreakdownChart =
-
-// TODO 3: TagHistogram: create a histogram for each category
-// const TagHistogram =
-
-// TODO 4: hide the histograms, and only show the relevant one when you click on a TagBreakdownChart bar
+const PL_CATEGORIES = {
+  PLANNED: { label: 'Planned', color: 'blue' },
+  UNPLANNED: { label: 'Unplanned', color: 'orange' },
+};
 
 function App() {
+  // hide the histogram, and only show it when you click on a BarChart bar
+  const [histogramPlCategory, setHistogramPlCategory] = useState(null);
   return (
     <div className="App">
-      <BarChart data={exampleData} />
+      <BarChart data={exampleData} plCategory={PL_CATEGORIES.PLANNED.label} />
+      {/* <BarChart data={exampleData} plCategory={PL_CATEGORIES.UNPLANNED.label} /> */}
+      {histogramPlCategory && (
+        <Histogram data={exampleData} plCategory={histogramPlCategory} />
+      )}
     </div>
   );
 }
